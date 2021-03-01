7b2e0fa3c50c6f561b567f76d68a804aac5ba95b
Sasindu umesh <45946252+coolsasindu@users.noreply.github.com>
2021-03-01 14:38:55 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,15 @@
+public class Arithmetic {
+  public static void main(String args[]) {
+
+    System.out.println(10+2); // 12
+    System.out.println(10-2); // 8
+    System.out.println(10/2); // 5
+    System.out.println(10*2); // 20
+
+    System.out.println(" " + 11%2); // 1
+
+    System.out.println(" " + 8/2); // 0
+
+    System.out.println(" " + 8%2); //0
+  }
+}
